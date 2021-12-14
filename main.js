4049b6f4f8be58d2d396a6abe66c5ece94bf7bfa
Johannes Mattia Winter <jojowillingen@gmail.com>
2021-12-14 01:11:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const configuration = {
   NumberOfDots: 5000,
   colors: {
     CanvasBackgroundColor: '#141414',
-    LettersColor: '#d8bdd',
+    LettersColor: '#c30068',
     LinesColors: ['#FFF', '#FF0000', '#7d7d7d'],
     LowerLinesColors: ['#3d3d3d'],
     DotsColor: '#7d7d7d'
